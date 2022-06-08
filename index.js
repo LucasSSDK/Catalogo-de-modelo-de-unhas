df0f328375d0387dc498a6eb786a1070db2444b2
LucasSSDK <l_ucass_oares@outlook.com>
2022-06-07 21:23:27 -0300

    Atualização

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require("express");
+const express = require
+
+("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+
+require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
